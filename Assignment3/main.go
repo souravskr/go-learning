9e270ad70931fc9a59ac818a0458e68b60572970
Sourav Sarker <souravsker@gmail.com>
2022-12-29 19:07:57 -0330

    Assignment 3

--- Assignment3/main.go
+++ Assignment3/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func main() {
+	inputFile := os.Args[1]
+	file, err := os.Open(inputFile)
+	check(err)
+
+	//bs := make([]byte, 9999)
+	//file.Read(bs)
+	//fmt.Println(string(bs))
+	io.Copy(os.Stdout, file)
+	fmt.Println()
+}
