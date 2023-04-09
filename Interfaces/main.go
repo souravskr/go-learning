7937342db609dba1e247ca8f854e17e36da45670
Sourav Sarker <souravsker@gmail.com>
2023-04-09 11:21:25 -0230

    Interface

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -4,39 +4,26 @@ import "fmt"
 
 type Animal interface {
 	says() string
-	numberOfLegs() int8
+	numberOfLegs() int
 }
 
 type Dog struct {
 	Name         string
 	Sounds       string
-	NumberOfLegs int8
+	NumberOfLegs int
 }
 
 type Cat struct {
 	Name         string
 	Sounds       string
-	NumberOfLegs int8
+	NumberOfLegs int
 	HasTail      bool
-}
-
-func main() {
-	dog := Dog{"Dog", "woof", 4}
-	cat := Cat{"Cat", "meow", 4, true}
-	riddle(&dog)
-	riddle(&cat)
-}
-
-func riddle(a Animal) {
-	riddle := fmt.Sprintf(`The animal has %d and it says %s, what animal is it?`, a.numberOfLegs, a.says)
-	fmt.Println(riddle)
 }
 
 func (d *Dog) says() string {
 	return d.Sounds
 }
-
-func (d *Dog) numberOfLegs() int8 {
+func (d *Dog) numberOfLegs() int {
 	return d.NumberOfLegs
 }
 
@@ -44,6 +31,18 @@ func (c *Cat) says() string {
 	return c.Sounds
 }
 
-func (c *Cat) numberOfLegs() int8 {
+func (c *Cat) numberOfLegs() int {
 	return c.NumberOfLegs
+}
+
+func riddle(a Animal) {
+	riddle := fmt.Sprintf(`This animal has %d legs and says %s. What animal is it?`, a.numberOfLegs(), a.says())
+	fmt.Println(riddle)
+}
+
+func main() {
+	cat := Cat{"Cat", "meow", 4, true}
+	dog := Dog{"Dog", "woof", 4}
+	riddle(&cat)
+	riddle(&dog)
 }
